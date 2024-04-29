0f7db647eeeaefb8acc8fd44742aa95d8d413894
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-04-29 17:37:44 -0300

    feat: add virtual fields to the project

--- src/Table.js
+++ src/Table.js
@@ -3,8 +3,14 @@ class Table {
     this.header = arr[0];
     arr.shift();
     this.rows = arr;
+  }
+  get RowCount() {
+    return this.rows.length;
     }
     
+    get ColumnCount() {
+      return this.header.length;
+    }
 }
 
 module.exports = Table;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ var Processor = require("./Processor");
 var Table = require("./Table");
 
 async function main() {
-    try {
-        var data = await reader.asyncRead("./src/CotizacionDolar.csv");
-       var dataProcessor= Processor.Process(data);
-        var user = new Table(dataProcessor);
-        console.log(user.header);
-    } catch (error) {
-        console.error("Error in main execution:", error);
-    }
+  try {
+    var data = await reader.asyncRead("./src/CotizacionDolar.csv");
+    var dataProcessor = Processor.Process(data);
+    var user = new Table(dataProcessor);
+    console.log(user.RowCount);
+  } catch (error) {
+    console.error("Error in main execution:", error);
+  }
 }
 
 main();
