bf6eccc411c5dcff75d0fbe84239a4e33726d63f
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-01 16:19:36 -0300

    fix: correct bugs in table

--- src/Table.js
+++ src/Table.js
@@ -1,16 +1,18 @@
+// Table.js
 class Table {
   constructor(arr) {
-    this.header = arr[0];
-    arr.shift();
-    this.rows = arr;
+    this.header = arr[0]; // Almacena la primera fila como cabecera
+    arr.shift();          // Elimina la primera fila del arreglo original
+    this.rows = arr;      // Almacena el resto de filas como datos
   }
+
   get RowCount() {
     return this.rows.length;
-    }
+  }
     
-    get ColumnCount() {
-      return this.header.length;
-    }
+  get ColumnCount() {
+    return this.header.length;
+  }
 }
 
 module.exports = Table;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-var Reader = require("./Reader");
-var reader = new Reader();
-var Processor = require("./Processor");
-var Table = require("./Table");
+// index.js
+const Table = require('./Table');
+const HtmlParser = require('./HtmlParser');
 
-async function main() {
-  try {
-    var data = await reader.asyncRead("./src/CotizacionDolar.csv");
-    var dataProcessor = Processor.Process(data);
-    var user = new Table(dataProcessor);
-    console.log(user.RowCount);
-  } catch (error) {
-    console.error("Error in main execution:", error);
-  }
-}
+let data = [
+    ['Column 1', 'Column 2', 'Column 3'], // Cabecera
+    ['Data 1', 'Data 2', 'Data 3'],       // Datos de la fila 1
+    ['Data 4', 'Data 5', 'Data 6']        // Datos de la fila 2
+];
 
-main();
+let table = new Table(data);
+
+HtmlParser.Parse(table)
+  .then(html => {
+    console.log(html);
+  })
+  .catch(err => {
+    console.error('Error:', err);
+  });
